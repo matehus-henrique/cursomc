532f49b9ab27c30a145b81a5c1e1e5eeaf93be59
matehus-henrique <mtheusriki@gmail.com>
2022-02-21 10:33:36 -0300

    testando a primeira classe categoria

--- src/main/java/com/example/demo/resouces/CategoriaResouces.java
+++ src/main/java/com/example/demo/resouces/CategoriaResouces.java
@@ -1,19 +1,31 @@
 package com.example.demo.resouces;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.domain.Categoria;
+
 @RestController
-@RequestMapping(value="/categorias")
+@RequestMapping(value = "/categorias")
 public class CategoriaResouces {
 
-	
 	@RequestMapping(method = RequestMethod.GET)
-	public String lista() {
+	public List<Categoria>  lista() {
+		Categoria cat1 = new Categoria(1, "Informatica");
+		Categoria cat2 = new Categoria(2, " Escritorio");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
 		
 		
-		return"rest esta funcionando";
+		
+		
+		return lista;
 	}
-	
+
 }

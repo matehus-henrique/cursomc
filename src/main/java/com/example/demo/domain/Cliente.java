faac3fe3c24c92959b1306517cd421b738afbde0
matehus-henrique <mtheusriki@gmail.com>
2022-02-23 14:09:48 -0300

    cliente e endereco

---
+++ src/main/java/com/example/demo/domain/Cliente.java
@@ -0,0 +1,125 @@
+package com.example.demo.domain;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+
+import javax.persistence.CollectionTable;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import com.example.demo.domain.enums.TipoCliente;
+@Entity
+public class Cliente implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String nome;
+	private String email;
+	private String cnpOUcnpj;
+	private Integer tipo;
+
+	@OneToMany(mappedBy = "cliente")
+	private List<Endereco> endereco = new ArrayList<>();
+	
+	@ElementCollection
+	@CollectionTable(name = "TELEFONE")
+	private Set<String> telefones = new HashSet<>();
+
+	public Cliente() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Cliente(Integer id, String nome, String email, String cnpOUcnpj, TipoCliente tipo) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+		this.cnpOUcnpj = cnpOUcnpj;
+		this.tipo = tipo.getCod();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getCnpOUcnpj() {
+		return cnpOUcnpj;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	public void setCnpOUcnpj(String cnpOUcnpj) {
+		this.cnpOUcnpj = cnpOUcnpj;
+	}
+
+	public TipoCliente getTipo() throws IllegalAccessException {
+		return TipoCliente.toEnum(tipo);
+	}
+
+	public void setTipo(TipoCliente tipo) {
+		this.tipo = tipo.getCod();
+	}
+
+	public List<Endereco> getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(List<Endereco> endereco) {
+		this.endereco = endereco;
+	}
+
+	public Set<String> getTelefones() {
+		return telefones;
+	}
+
+	public void setTelefones(Set<String> telefones) {
+		this.telefones = telefones;
+	}
+
+}

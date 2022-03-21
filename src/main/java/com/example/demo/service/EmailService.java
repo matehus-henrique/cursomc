d4e0e4cfa699096108cd313e1e6a1cabc72d337e
matehus-henrique <mtheusriki@gmail.com>
2022-03-21 14:15:27 -0300

    emails com html

--- src/main/java/com/example/demo/service/EmailService.java
+++ src/main/java/com/example/demo/service/EmailService.java
@@ -1,15 +1,19 @@
 package com.example.demo.service;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.example.demo.domain.Pedido;
 
 public interface EmailService {
-	
+
 	void sendOrderConfimationEmail(Pedido obj);
-	
+
 	void sendEmail(SimpleMailMessage msg);
 
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+
+	void sendHtmlEmail(MimeMessage msg);
+
 }
-
-
--- src/main/java/com/example/demo/service/MockEmailService.java
+++ src/main/java/com/example/demo/service/MockEmailService.java
@@ -1,7 +1,8 @@
 package com.example.demo.service;
 
-import org.slf4j.Logger;
+import javax.mail.internet.MimeMessage;
 
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.mail.SimpleMailMessage;
 
@@ -16,6 +17,13 @@ public class MockEmailService extends AbstractEmailService {
 		LOG.info(msg.toString());
 		LOG.info("Email enviado");
 		
+	}
+
+	@Override
+	public void sendHtmlEmail(MimeMessage msg) {
+		LOG.info("Simulando envio de email HTML ...");
+		LOG.info(msg.toString());
+		LOG.info("Email enviado");	
 	}
 
 }

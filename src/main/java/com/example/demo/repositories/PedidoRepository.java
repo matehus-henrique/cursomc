4664b6b871a6bf3ed8c33844fc662779d4966b6f
matehus-henrique <mtheusriki@gmail.com>
2022-03-31 15:36:49 -0300

    Restrição de conteúdo: cliente só recupera seus pedidos -- algo estranho

--- src/main/java/com/example/demo/repositories/PedidoRepository.java
+++ src/main/java/com/example/demo/repositories/PedidoRepository.java
@@ -1,12 +1,20 @@
 package com.example.demo.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.example.demo.domain.Cliente;
 import com.example.demo.domain.Pedido;
 
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
 // um obj desse pode gera vairas coisas busca salva
+	
+	@Transactional(readOnly = true)
+	Page<Pedido> findByCliente(Cliente cliente, Pageable pageRequrst );
+	
 }
--- src/main/java/com/example/demo/service/PedidoService.java
+++ src/main/java/com/example/demo/service/PedidoService.java
@@ -4,9 +4,13 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.example.demo.domain.Cliente;
 import com.example.demo.domain.ItemPedido;
 import com.example.demo.domain.PagamentoComBoleto;
 import com.example.demo.domain.Pedido;
@@ -14,6 +18,8 @@ import com.example.demo.domain.enums.EstadoPagamento;
 import com.example.demo.repositories.ItemPedidoRepository;
 import com.example.demo.repositories.PagamentoRepository;
 import com.example.demo.repositories.PedidoRepository;
+import com.example.demo.security.UserSS;
+import com.example.demo.service.exception.AuthorizationException;
 import com.example.demo.service.exception.ObjectNotFoundException;
 
 @Service
@@ -71,7 +77,17 @@ public class PedidoService {
 		emailService.sendOrderConfirmationHtmlEmail(obj);
 		return obj;
 	}
-
+	
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		UserSS user = UserService.authenticated();
+		if (user == null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente =  ClienteService.find(user.getId());
+		return repo.findByCliente(cliente, pageRequest);
+		
+	}
 	
 }
 

0902c4b27319e523b824e639b0534252aad6e4a6
matehus-henrique <mtheusriki@gmail.com>
2022-04-06 13:55:52 -0300

    Salvando primeiro arquivo no S3

--- src/main/java/com/example/demo/CursomcApplication.java
+++ src/main/java/com/example/demo/CursomcApplication.java
@@ -1,13 +1,17 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.example.demo.service.S3Service;
+
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 	
-	
+	@Autowired
+	private S3Service s3Service;
 	
 
 	
@@ -19,7 +23,7 @@ public class CursomcApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		
 	
-		
+		s3Service.uploadFile("C:\\laravel_projeto\\projeto1\\public\\img\\banner.jpg");
 		
 	}
 

eec0bd244dbf48a9186c743cc3208116779313fc
matehus-henrique <mtheusriki@gmail.com>
2022-04-11 10:58:06 -0300

    ajustando imagen

--- src/main/java/com/example/demo/service/ClienteService.java
+++ src/main/java/com/example/demo/service/ClienteService.java
@@ -54,6 +54,9 @@ public class ClienteService {
 	@Value("${img.prefix.client.profile}")
 	private String prefix;
 	
+	@Value("${img.profile.size}")
+	private Integer size;
+	
 	// find = a buscar.
 	public Cliente busca(Integer id) {
 		UserSS user = UserService.authenticated();
@@ -150,6 +153,9 @@ public class ClienteService {
 		}
 		
 		BufferedImage jpgImage = imageService.getJpgImageFromFile(multipartFile);
+		jpgImage = imageService.cropSquare(jpgImage);
+		jpgImage = imageService.resize(jpgImage, size);
+		
 		String fileName = prefix + user.getId() + ".jpg";
 		
 		return s3service.uploadFile(imageService.getInputStream(jpgImage, "jpg"), fileName, "image");
